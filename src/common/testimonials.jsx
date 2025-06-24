fc3db3efc57e0ea0fcda0968c5ffddcf4ff16025
digiexpouae <tech.digiexpo@gmail.com>
2025-06-24 13:42:44 +0500

    testimonials fixed

--- src/common/testimonials.jsx
+++ src/common/testimonials.jsx
@@ -10,14 +10,14 @@ const testimonials = () => {
   ,name:'Emily P.',image:'/assets/testi2.png',role:'House Wife'}]
   return (
     <div className='w-full h-[300px] md:h-[500px] lg:h-[600px] flex items-center justify-center'>
-      <div className='flex items-center justify-center md:gap-24 w-[90%] h-[70%] md:w-[75%] lg:w-[70%] relative bg-white rounded-2xl'>
+      <div className='flex items-center justify-center md:gap-24 w-[90%] h-[70%] md:w-[80%] xl:w-[70%] relative bg-white rounded-2xl'>
        
-       <div className='absolute w-[30%] md:w-[35%] h-[150px] md:h-[500px] -top-5 md:-top-20 left-0'>
-         <div className='h-[230px] md:h-[430px] lg:h-[500px]  relative overflow-hidden testi'><Image src={burj}  className='!h-[100%] !w-full object-cover   rounded-bl-2xl [object-position:80%_center] xl:[object-position:95%_center]' 
+       <div className='absolute w-[35%] md:w-[35%] h-[150px] md:h-[500px] -top-5 md:-top-20 left-0'>
+         <div className='h-[230px] md:h-[430px] lg:h-[500px]  relative overflow-hidden testi'><Image src={burj}  className='!h-[100%] !w-full object-cover   rounded-bl-2xl [object-position:85%_center] md:[object-position:85%_center] xl:[object-position:95%_center]' 
          />
          </div>
          </div>
-        <div className=' w-[70%] md:w-[65%] absolute right-0'>
+        <div className='w-[65%] absolute right-0'>
         <Testimonial testimonial={testimonial}/></div>
         </div>
     </div>
