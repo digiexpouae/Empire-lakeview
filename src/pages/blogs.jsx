e8d2cdd80123cbbdacf8ee421b41adeeae488166
Ahmedgito <mamooabid1@gmail.com>
2025-07-07 14:01:51 +0500

    images fixed

--- src/pages/blogs.jsx
+++ src/pages/blogs.jsx
@@ -49,7 +49,7 @@ const Blogs = () => {
             blogs.map((blog) => (
               <Link key={blog.slug} href={`/blogs/${blog.slug}`} className="group">
                 <div className="bg-[#23262F] rounded-3xl shadow-md overflow-hidden flex flex-col h-full transition-transform duration-200 hover:-translate-y-1 hover:shadow-xl border border-[#23262F] hover:border-[#CCAB64]">
-                  <div className="md:h-full h-full w-full overflow-hidden bg-gray-800 flex items-center justify-center">
+                  <div className="md:h-64 h-full w-full overflow-hidden bg-gray-800 flex items-center justify-center">
                     {blog.Image ? (
                       <img 
                         src={urlFor(blog.Image).url()} 
