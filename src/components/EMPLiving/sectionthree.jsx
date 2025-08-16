4ddf32af38c7bbfd34fbfcffc49e1f5b138116c8
Ahmedgito <mamooabid1@gmail.com>
2025-08-16 14:09:42 +0500

    Fixed

--- src/components/EMPLiving/sectionthree.jsx
+++ src/components/EMPLiving/sectionthree.jsx
@@ -32,113 +32,94 @@ const SectionThree = () => {
   useLayoutEffect(() => {
     let ctx;
 
-  import('gsap').then(gsapModule => {
-    import('gsap/ScrollTrigger').then(scrollTriggerModule => {
-      const gsap = gsapModule.default;
-      const ScrollTrigger = scrollTriggerModule.ScrollTrigger;
-      gsap.registerPlugin(ScrollTrigger);
+    import('gsap').then(gsapModule => {
+      import('gsap/ScrollTrigger').then(scrollTriggerModule => {
+        const gsap = gsapModule.default;
+        const ScrollTrigger = scrollTriggerModule.ScrollTrigger;
+        gsap.registerPlugin(ScrollTrigger);
 
-      if (!scrollRef.current || !container.current) return;
+        if (!scrollRef.current || !container.current) return;
 
-      ctx = gsap.context(() => {
-        const scrollEl = scrollRef.current;
-        const scrollHeight = scrollEl.scrollHeight ;
-        const containerHeight = container.current.offsetHeight;
-        const scrollDistance = (scrollHeight + 100) - containerHeight;
-      const ending=scrollDistance + 500
-        // Set scrollTop to 0 zinitially
-        scrollEl.scrollTop = 0;
+        ctx = gsap.context(() => {
+          const scrollEl = scrollRef.current;
+          const scrollHeight = scrollEl.scrollHeight;
+          const containerHeight = container.current.offsetHeight;
+          const scrollDistance = (scrollHeight + 100) - containerHeight;
+          const ending = scrollDistance + 500;
+          
+          // Set scrollTop to 0 initially
+          scrollEl.scrollTop = 0;
 
-        // Create the scroll animation
-  const ct= gsap.to(scrollRef.current, {
-          scrollTop: ending,
-          ease: "none",
-          scrollTrigger: {
-            trigger: container.current,
-            start: "center center",
-            end: `+=${scrollDistance}`,
-            scrub:2.0,
-            pin: true,
-            pinType:'transform',
-            pinSpacing: true,
-            anticipatePin: 1,
-           
-            onLeave: () => {
-    document.querySelector('.container').style.position = 'relative';
-  },
-      
-
-          }
-        });
-      }, container);
+          // Create the scroll animation
+          const ct = gsap.to(scrollRef.current, {
+            scrollTop: ending,
+            ease: "none",
+            scrollTrigger: {
+              trigger: container.current,
+              start: "center center",
+              end: `+=${scrollDistance}`,
+              scrub: 2.0,
+              pin: true,
+              pinType: 'transform',
+              pinSpacing: true,
+              anticipatePin: 1,
+              onLeave: () => {
+                document.querySelector('.container').style.position = 'relative';
+              },
+            }
+          });
+        }, container);
+      });
     });
-  });
 
-  return () => {
-    // 👇 protect against undefined
-    if (ctx && ctx.kill) {
-      ctx.kill();
-    }
-  };
-}, [container]);
-
-
-
+    return () => {
+      if (ctx && ctx.kill) {
+        ctx.kill();
+      }
+    };
+  }, [container]);
 
   return (
-  <div className=' flex flex-col  items-center justify-center w-full text-center h-[100vh] z-[10] relative' ref={container}>
-  
-    <h1 className='text-[45px] lg:text-[100px] my-heading  text-white font-bold'>Picture Your Future</h1>
-    <div className='w-full h-screen overflow-hidden scrollbar-hide flex flex-col  items-center justify-center'    ref={scrollRef}  >
-
-          <div className='w-[90%] md:w-[80%] h-full flex items-center justify-center'>
-    
-
-            <div className='columns-4 flex flex-wrap h-full m-auto items-center justify-center gap-3'>
-    
-    <div className={'mt-20 w-[22%] loading="lazy" h-[110px] md:h-[190px] lg:h-[290px]'}><Image src={empire1}  className='!h-full !w-full   rounded-3xl object-cover object-center'/></div>
-    <div className={'w-[22%] loading="lazy" h-[120px] md:h-[200px] lg:h-[280px] '}><Image src={empire18}    className='!h-full !w-full  rounded-3xl object-cover '/></div>
-    <div className={'w-[22%] loading="lazy" h-[90px] md:h-[170px] lg:h-[250px]  mt-16 '}><Image src={empire3}   className='!h-full !w-full  rounded-3xl object-cover '/></div>
-    <div className={'w-[22%] loading="lazy" h-[120px] md:h-[200px] lg:h-[290px]  '}><Image src={empire4}   className='!h-full !w-full   rounded-3xl object-cover '/></div>
-    <div className={'w-[22%] loading="lazy" h-[100px] md:h-[180px] lg:h-[260px] -mt-2 md:-mt-10'}><Image src={empire5}   className='!h-full !w-full   rounded-3xl object-cover '/></div>
-    <div className={'w-[22%] loading="lazy" h-[140px] md:h-[270px] lg:h-[350px] -mt-10  md:-mt-10 lg:-mt-12'}><Image src={empire6}    className='!h-full !w-full   rounded-3xl object-cover '/></div>
-    <div className={'w-[22%] loading="lazy" h-[80px] md:h-[160px] lg:h-[240px] -mt-12 md:-mt-30 '}><Image src={empire7}   className='!h-full !w-full   rounded-3xl object-cover '/></div>
-    <div className={'w-[22%] loading="lazy" h-[120px] md:h-[200px] lg:h-[290px] -mt-16 md:-mt-24'}><Image src={empire8}    className='!h-full !w-full   rounded-3xl object-cover'/></div>
-    <div className={'w-[22%] loading="lazy"  h-[140px] md:h-[270px] lg:h-[350px] -mt-2 md:-mt-12 lg:-mt-10 '}><Image src={empire9}    className='!h-full !w-full  rounded-3xl object-cover'/></div>
-    <div className={'w-[22%] loading="lazy" h-[100px] md:-[180px] lg:h-[260px] md:-mt-28 lg:-mt-12'}><Image src={empire10}   className='!h-full !w-full   rounded-3xl object-cover'/></div>
-    <div className={'w-[22%] loading="lazy" h-[60px] md:h-[140px] lg:h-[320px] -mt-32 md:-mt-70 lg:-mt-42'}><Image src={empire2}    className='!h-full !w-full   rounded-3xl object-cover'/></div>
-    <div className={'w-[22%] loading="lazy" h-[100px] md:h-[180px] lg:h-[260px] -mt-16 md:-mt-38'}><Image src={empire12}  className='!h-full !w-full   rounded-3xl object-cover'/></div>
-    <div className={'w-[22%] loading="lazy"  h-[140px] md:h-[270px] lg:h-[350px]  '}><Image src={empireliv}    className='!h-full !w-full   rounded-3xl object-cover'/></div>
-    <div className={'w-[22%] loading="lazy" h-[90px] md:h-[170px] lg:h-[250px]  -mt-20 md:-mt-84 lg:-mt-46 '}><Image src={empire14}   className='!h-full !w-full   rounded-3xl object-cover'/></div>
-    <div className={'w-[22%] loading="lazy"  h-[140px] md:h-[270px] lg:h-[350px] lg:-mt-38 md:-mt-90 -mt-40'}><Image src={empire15}   className='!h-full !w-full   rounded-3xl object-cover'/></div>
-    <div className={'w-[22%] loading="lazy"  h-[140px] md:h-[270px] lg:h-[350px] -mt-20 md:-mt-52'}><Image src={empire16}   className='!h-full !w-full   rounded-3xl object-cover'/></div>
-    <div className={'w-[22%] loading="lazy" h-[90px] md:h-[170px] lg:h-[250px] -mt-4 md:-mt-2'}><Image src={empire3}    className='!h-full !w-full   rounded-3xl object-cover'/></div>
-    <div className={'w-[22%] loading="lazy"  h-[140px] md:h-[270px] lg:h-[350px] -mt-16 md:-mt-84 lg:-mt-54'}><Image src={empire2}  className='!h-full !w-full   rounded-3xl object-cover'/></div> 
-         <div className={'w-[22%] loading="lazy"  h-[140px] md:h-[270px] lg:h-[350px] -mt-26 md:-mt-65 lg:-mt-18'}><Image src={empire8}   className='!h-full !w-full   rounded-3xl object-cover'/></div> 
-
-     <div className={'w-[22%] loading="lazy"  h-[140px] md:h-[270px] lg:h-[350px] -mt-8 md:-mt-24'}><Image   src={empire13}  className='!h-full !w-full   rounded-3xl object-cover'/></div> 
-     <div className={'w-[22%] loading="lazy" h-[120px] md:h-[200px] lg:h-[290px] '}><Image src={empire}   className='!h-full !w-full   rounded-3xl object-cover'/></div>
-         <div className='flex flex-col items-center  w-[45%]'>
-          <div className={'w-[90%] h-[120px] md:h-[150px] -mt-8 '}><Image src={empirelivin}   className='!h-full !w-full   rounded-3xl object-contain'/>
+    <div className='flex flex-col items-center justify-center w-full text-center h-[100vh] z-[10] relative' ref={container} style={{ transform: 'translate3d(0,0,0)', willChange: 'transform' }}>
+      <h1 className='text-[45px] lg:text-[100px] my-heading text-white font-bold' style={{ transform: 'translate3d(0,0,0)' }}>Picture Your Future</h1>
+      <div className='w-full h-screen overflow-hidden scrollbar-hide flex flex-col items-center justify-center' ref={scrollRef} style={{ transform: 'translate3d(0,0,0)', willChange: 'transform' }}>
+        <div className='w-[90%] md:w-[80%] h-full flex items-center justify-center' style={{ transform: 'translate3d(0,0,0)' }}>
+          <div className='columns-4 flex flex-wrap h-full m-auto items-center justify-center gap-3' style={{ transform: 'translate3d(0,0,0)', backfaceVisibility: 'hidden', perspective: '1000px' }}>
+            <div className={'mt-20 w-[22%] loading="lazy" h-[110px] md:h-[190px] lg:h-[290px]'} style={{ transform: 'translate3d(0,0,0)', backfaceVisibility: 'hidden' }}><Image src={empire1} className='!h-full !w-full rounded-3xl object-cover object-center' style={{ transform: 'translate3d(0,0,0)' }} /></div>
+            <div className={'w-[22%] loading="lazy" h-[120px] md:h-[200px] lg:h-[280px]'} style={{ transform: 'translate3d(0,0,0)', backfaceVisibility: 'hidden' }}><Image src={empire18} className='!h-full !w-full rounded-3xl object-cover' style={{ transform: 'translate3d(0,0,0)' }} /></div>
+            <div className={'w-[22%] loading="lazy" h-[90px] md:h-[170px] lg:h-[250px] mt-16'} style={{ transform: 'translate3d(0,0,0)', backfaceVisibility: 'hidden' }}><Image src={empire3} className='!h-full !w-full rounded-3xl object-cover' style={{ transform: 'translate3d(0,0,0)' }} /></div>
+            <div className={'w-[22%] loading="lazy" h-[120px] md:h-[200px] lg:h-[290px]'} style={{ transform: 'translate3d(0,0,0)', backfaceVisibility: 'hidden' }}><Image src={empire4} className='!h-full !w-full rounded-3xl object-cover' style={{ transform: 'translate3d(0,0,0)' }} /></div>
+            <div className={'w-[22%] loading="lazy" h-[100px] md:h-[180px] lg:h-[260px] -mt-2 md:-mt-10'} style={{ transform: 'translate3d(0,0,0)', backfaceVisibility: 'hidden' }}><Image src={empire5} className='!h-full !w-full rounded-3xl object-cover' style={{ transform: 'translate3d(0,0,0)' }} /></div>
+            <div className={'w-[22%] loading="lazy" h-[140px] md:h-[270px] lg:h-[350px] -mt-10 md:-mt-10 lg:-mt-12'} style={{ transform: 'translate3d(0,0,0)', backfaceVisibility: 'hidden' }}><Image src={empire6} className='!h-full !w-full rounded-3xl object-cover' style={{ transform: 'translate3d(0,0,0)' }} /></div>
+            <div className={'w-[22%] loading="lazy" h-[80px] md:h-[160px] lg:h-[240px] -mt-12 md:-mt-30'} style={{ transform: 'translate3d(0,0,0)', backfaceVisibility: 'hidden' }}><Image src={empire7} className='!h-full !w-full rounded-3xl object-cover' style={{ transform: 'translate3d(0,0,0)' }} /></div>
+            <div className={'w-[22%] loading="lazy" h-[120px] md:h-[200px] lg:h-[290px] -mt-16 md:-mt-24'} style={{ transform: 'translate3d(0,0,0)', backfaceVisibility: 'hidden' }}><Image src={empire8} className='!h-full !w-full rounded-3xl object-cover' style={{ transform: 'translate3d(0,0,0)' }} /></div>
+            <div className={'w-[22%] loading="lazy" h-[140px] md:h-[270px] lg:h-[350px] -mt-2 md:-mt-12 lg:-mt-10'} style={{ transform: 'translate3d(0,0,0)', backfaceVisibility: 'hidden' }}><Image src={empire9} className='!h-full !w-full rounded-3xl object-cover' style={{ transform: 'translate3d(0,0,0)' }} /></div>
+            <div className={'w-[22%] loading="lazy" h-[100px] md:-[180px] lg:h-[260px] md:-mt-28 lg:-mt-12'} style={{ transform: 'translate3d(0,0,0)', backfaceVisibility: 'hidden' }}><Image src={empire10} className='!h-full !w-full rounded-3xl object-cover' style={{ transform: 'translate3d(0,0,0)' }} /></div>
+            <div className={'w-[22%] loading="lazy" h-[60px] md:h-[140px] lg:h-[320px] -mt-32 md:-mt-70 lg:-mt-42'} style={{ transform: 'translate3d(0,0,0)', backfaceVisibility: 'hidden' }}><Image src={empire2} className='!h-full !w-full rounded-3xl object-cover' style={{ transform: 'translate3d(0,0,0)' }} /></div>
+            <div className={'w-[22%] loading="lazy" h-[100px] md:h-[180px] lg:h-[260px] -mt-16 md:-mt-38'} style={{ transform: 'translate3d(0,0,0)', backfaceVisibility: 'hidden' }}><Image src={empire12} className='!h-full !w-full rounded-3xl object-cover' style={{ transform: 'translate3d(0,0,0)' }} /></div>
+            <div className={'w-[22%] loading="lazy" h-[140px] md:h-[270px] lg:h-[350px]'} style={{ transform: 'translate3d(0,0,0)', backfaceVisibility: 'hidden' }}><Image src={empireliv} className='!h-full !w-full rounded-3xl object-cover' style={{ transform: 'translate3d(0,0,0)' }} /></div>
+            <div className={'w-[22%] loading="lazy" h-[90px] md:h-[170px] lg:h-[250px] -mt-20 md:-mt-84 lg:-mt-46'} style={{ transform: 'translate3d(0,0,0)', backfaceVisibility: 'hidden' }}><Image src={empire14} className='!h-full !w-full rounded-3xl object-cover' style={{ transform: 'translate3d(0,0,0)' }} /></div>
+            <div className={'w-[22%] loading="lazy" h-[140px] md:h-[270px] lg:h-[350px] lg:-mt-38 md:-mt-90 -mt-40'} style={{ transform: 'translate3d(0,0,0)', backfaceVisibility: 'hidden' }}><Image src={empire15} className='!h-full !w-full rounded-3xl object-cover' style={{ transform: 'translate3d(0,0,0)' }} /></div>
+            <div className={'w-[22%] loading="lazy" h-[140px] md:h-[270px] lg:h-[350px] -mt-20 md:-mt-52'} style={{ transform: 'translate3d(0,0,0)', backfaceVisibility: 'hidden' }}><Image src={empire16} className='!h-full !w-full rounded-3xl object-cover' style={{ transform: 'translate3d(0,0,0)' }} /></div>
+            <div className={'w-[22%] loading="lazy" h-[90px] md:h-[170px] lg:h-[250px] -mt-4 md:-mt-2'} style={{ transform: 'translate3d(0,0,0)', backfaceVisibility: 'hidden' }}><Image src={empire3} className='!h-full !w-full rounded-3xl object-cover' style={{ transform: 'translate3d(0,0,0)' }} /></div>
+            <div className={'w-[22%] loading="lazy" h-[140px] md:h-[270px] lg:h-[350px] -mt-16 md:-mt-84 lg:-mt-54'} style={{ transform: 'translate3d(0,0,0)', backfaceVisibility: 'hidden' }}><Image src={empire2} className='!h-full !w-full rounded-3xl object-cover' style={{ transform: 'translate3d(0,0,0)' }} /></div>
+            <div className={'w-[22%] loading="lazy" h-[140px] md:h-[270px] lg:h-[350px] -mt-26 md:-mt-65 lg:-mt-18'} style={{ transform: 'translate3d(0,0,0)', backfaceVisibility: 'hidden' }}><Image src={empire8} className='!h-full !w-full rounded-3xl object-cover' style={{ transform: 'translate3d(0,0,0)' }} /></div>
+            <div className={'w-[22%] loading="lazy" h-[140px] md:h-[270px] lg:h-[350px] -mt-8 md:-mt-24'} style={{ transform: 'translate3d(0,0,0)', backfaceVisibility: 'hidden' }}><Image src={empire13} className='!h-full !w-full rounded-3xl object-cover' style={{ transform: 'translate3d(0,0,0)' }} /></div>
+            <div className={'w-[22%] loading="lazy" h-[120px] md:h-[200px] lg:h-[290px]'} style={{ transform: 'translate3d(0,0,0)', backfaceVisibility: 'hidden' }}><Image src={empire} className='!h-full !w-full rounded-3xl object-cover' style={{ transform: 'translate3d(0,0,0)' }} /></div>
+            <div className='flex flex-col items-center w-[45%]' style={{ transform: 'translate3d(0,0,0)', backfaceVisibility: 'hidden' }}>
+              <div className={'w-[90%] h-[120px] md:h-[150px] -mt-8'} style={{ transform: 'translate3d(0,0,0)' }}><Image src={empirelivin} className='!h-full !w-full rounded-3xl object-contain' style={{ transform: 'translate3d(0,0,0)' }} />
+              </div>
+              <a
+                href="/files/02 - Individual Floor Plans Book - Empire Livings.pdf"
+                download
+                className="h-[30px] md:h-auto cursor-pointer flex items-center justify-center bg-gradient-to-r from-[#CCAB64] to-[#FAECC9] hover:from-[#E5C98C] hover:to-[#E5C98C] text-gray-800 w-[120px] md:w-[180px] lg:px-2 py-1 rounded-full transition-colors duration-300 transform text-[9px] md:text-sm lg:text-base whitespace-nowrap" style={{ transform: 'translate3d(0,0,0)' }}>Download Floor Plan</a>
             </div>
-                   <a 
-                  href="/files/02 - Individual Floor Plans Book - Empire Livings.pdf"
-                  download
-                   className=" h-[30px] md:h-auto cursor-pointer flex items-center justify-center
-  bg-gradient-to-r from-[#CCAB64] to-[#FAECC9]
-    hover:from-[#E5C98C] hover:to-[#E5C98C] text-gray-800 w-[120px] md:w-[180px] lg:px-2 py-1 rounded-full transition-colors duration-300  transform text-[9px] md:text-sm lg:text-base whitespace-nowrap">Download Floor Plan</a>
-          </div>
-
-    <div className={'w-[22%] loading="lazy" h-[120px] md:h-[180px] lg:h-[260px] '}><Image src={empire19}   className='!h-full !w-full   rounded-3xl object-cover'/></div>
-    {/* <div><Image src={empire}  className='!h-full !w-full   rounded-3xl object-cover'/></div> */}
-    
+            <div className={'w-[22%] loading="lazy" h-[120px] md:h-[180px] lg:h-[260px]'} style={{ transform: 'translate3d(0,0,0)', backfaceVisibility: 'hidden' }}><Image src={empire19} className='!h-full !w-full rounded-3xl object-cover' style={{ transform: 'translate3d(0,0,0)' }} /></div>
           </div>
         </div>
-
-    </div>
-        <div className='absolute left-0 right-0 bottom-0 w-full md:h-[50px] lg:h-[50px]'style={{background: 'linear-gradient(180deg, rgba(0, 46, 60, 0) 50%, #0E1527 97%)'
-}}></div>
       </div>
+      <div className='absolute left-0 right-0 bottom-0 w-full md:h-[50px] lg:h-[50px]' style={{ background: 'linear-gradient(180deg, rgba(0, 46, 60, 0) 50%, #0E1527 97%)', transform: 'translate3d(0,0,0)' }}></div>
+    </div>
   )
 }
 
