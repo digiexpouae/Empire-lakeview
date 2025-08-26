bbd71638c3cc11bd8cb47cc473cb6fd0304d036b
ramsha1993 <rimshasaleem1993@gmail.com>
2025-08-26 09:56:01 +0500

    map changes

--- src/common/maptwo.jsx
+++ src/common/maptwo.jsx
@@ -4,7 +4,7 @@ const maptwo = ({title,map,ifrmae_map}) => {
   return (
     <div className='flex flex-col items-center justify-center h-[400px] md:h-[700px] my-heading'>
         <div className='w-full text-center '>
-          <h2 className='xs:leading-17 mb-5 md:mb-10 text-[30px] md:text-[40px] lg:text-[60px] my-heading  font-bold text-white'>{title}</h2>
+          <h2 className='xs:leading-17 mb-2 text-[25px] md:text-[40px] lg:text-[60px] my-heading  font-bold text-white'>{title}</h2>
           </div>
     <div className='  xl:!h-[500px] lg:!h-[450px] md:!h-[300px] w-[90vw] md:w-[75vw] !h-[200px] relative'>
      
