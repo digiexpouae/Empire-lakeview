a82cc43c5e052ba499a3802318be9464214a4e70
Ahmedgito <mamooabid1@gmail.com>
2025-08-27 06:29:53 +0500

    re push

--- src/pages/api/contact.js
+++ src/pages/api/contact.js
@@ -49,7 +49,7 @@ export default async function handler(req, res) {
     });
 
 
-    return res.status(200).json({ message: 'Email sent successfully' });
+    return res.status(200).json({ message: 'Email sent successfully.' });
   } catch (error) {
     return res.status(500).json({ message: 'Failed to send email', error: error.message });
   }
